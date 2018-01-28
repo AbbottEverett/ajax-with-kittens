f4690cc434bef8c98b6e2f1170c9f3cf81153006
S. Everett Abbott <severettabbott@gmail.com>
2018-01-28 13:17:03 -0800

    Built project to requirements

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-axios.get('http://localhost:3000/cats')
-  .then(result => {
-    const cats = result.data.data
-    const items = cats.reduce((acc, cat) => `<li>${cat.name}</li>`, '')
-    document.querySelector('ul').innerHTML = items
-  })
-  .catch(err => {
-    console.log('Do not worry, everything is fine.')
-  })
+function getAllCats() {
+  axios.get('http://localhost:3000/cats')
+    .then(result => {
+      const cats = result.data.data
+      const items = cats.reduce((acc, cat) => acc + `<li>${cat.name}</li>`, '')
+      document.querySelector('ul').innerHTML = items
+    })
+    .catch(err => {
+      console.log('Do not worry, everything is fine.')
+    });
+}
+
+function createCat(text){
+  axios.post('http://localhost:3000/cats', { name: text })
+    .then((data) => {
+      getAllCats();
+    });
+}
+
+const createButton = document.getElementById('posts-create');
+const input = document.getElementById('new-cat-input');
+const form = document.getElementById('new-cat-form');
+
+createButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  input.style.display = "initial";
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const val = input.value;
+  input.value = '';
+  input.style.display = "none";
+  createCat(val);
+});
